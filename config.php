5e2c593f351961c12a2ee36756d44a5e3e8a7326
Rodrigo Aravena 898 <82234888+rodrigog898@users.noreply.github.com>
2023-12-28 16:42:57 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+// CONFIGURACION DE GOOGLE
+$clientID = '752395808634-32hthro9tg0mfm9latnrf8f98djda91t.apps.googleusercontent.com';
+$clientSecret = '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';
+$redirectUri = 'https://rodrigog898.github.io/main ';
